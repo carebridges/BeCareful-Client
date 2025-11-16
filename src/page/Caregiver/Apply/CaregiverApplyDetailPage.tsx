97649c39c7bdf3f29efb4f23616ce59b2c7d2947
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-11-16 19:43:36 +0900

    fix: 요양보호사 지원현황 화면 수정

--- src/page/Caregiver/Apply/CaregiverApplyDetailPage.tsx
+++ src/page/Caregiver/Apply/CaregiverApplyDetailPage.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import CaregiverWorkDetail from '@/components/Caregiver/CaregiverWorkDetail';
+import { Button } from '@/components/common/Button/Button';
+import { useHandleNavigate } from '@/hooks/useHandleNavigate';
 import { useApplicationDetailQuery } from '@/api/caregiver';
 
 const CaregiverApplyDetailPage = () => {
   const { recruitmentId } = useParams();
+
+  const { handleNavigate } = useHandleNavigate();
 
   const { data, error } = useApplicationDetailQuery(Number(recruitmentId));
   if (error) {
@@ -17,10 +21,29 @@ const CaregiverApplyDetailPage = () => {
 
   return (
     <Container>
-      <CaregiverWorkDetail
-        work={data.recruitmentDetailInfo}
-        date={data.applyDate}
-      />
+      <CaregiverWorkDetail work={data.recruitmentDetailInfo} />
+
+      <Bottom>
+        <Button
+          height="52px"
+          variant={
+            data.recruitmentDetailInfo.recruitmentInfo.recruitmentStatus ===
+            '공고마감'
+              ? 'disabled'
+              : 'mainBlue'
+          }
+          disabled={
+            data.recruitmentDetailInfo.recruitmentInfo.recruitmentStatus ===
+            '공고마감'
+          }
+          onClick={() => handleNavigate('/chat')}
+        >
+          {data.recruitmentDetailInfo.recruitmentInfo.recruitmentStatus ===
+          '공고마감'
+            ? '마감된 공고입니다'
+            : '채팅방으로 이동하기'}
+        </Button>
+      </Bottom>
     </Container>
   );
 };
@@ -28,3 +51,15 @@ const CaregiverApplyDetailPage = () => {
 export default CaregiverApplyDetailPage;
 
 const Container = styled.div``;
+
+const Bottom = styled.div`
+  padding: 20px;
+  display: flex;
+  background: ${({ theme }) => theme.colors.white};
+  border-top: 1px solid ${({ theme }) => theme.colors.gray50};
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
