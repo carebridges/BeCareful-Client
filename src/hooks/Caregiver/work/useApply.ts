da456728a92fbcd19deab824ab684936151763bd
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-11-16 19:33:30 +0900

    fix: 요양보호사 일자리 상세조회 관련 수정

--- src/hooks/Caregiver/work/useApply.ts
+++ src/hooks/Caregiver/work/useApply.ts
@@ -1,20 +1,31 @@
 import { useState } from 'react';
 import { usePostApplyMutation } from '@/hooks/Caregiver/mutation/useApplyMutation';
+import { handleModal } from '@/utils/handleModal';
 
 export const useApply = (recruitmentId: number) => {
   // 지원하기 팝업
   const [isApplyModalOpen, setIsApplyModalOpen] = useState(false);
-  const handleCompleteApply = () => {
-    setIsApplyModalOpen(true);
-    applyMutation();
-  };
+
+  // 지원 완료 팝업
+  const [isCompleteApplyModalOpen, setIsCompleteApplyModalOpen] =
+    useState(false);
 
   // 매칭 공고 지원
-  const { mutate: applyMutation } = usePostApplyMutation(recruitmentId);
+  const { mutate: applyMutation } = usePostApplyMutation(recruitmentId, {
+    onSuccessCallback: () =>
+      handleModal(setIsCompleteApplyModalOpen, setIsApplyModalOpen),
+  });
+
+  // 지원하기
+  const handleCompleteApply = () => {
+    applyMutation();
+  };
 
   return {
     isApplyModalOpen,
     setIsApplyModalOpen,
+    isCompleteApplyModalOpen,
+    setIsCompleteApplyModalOpen,
     handleCompleteApply,
   };
 };
