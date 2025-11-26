9d91b87756d7a7f32fb9fe13e367781305c87389
Lee Jungmin <99460302+mungjin01@users.noreply.github.com>
2025-11-26 14:12:22 +0900

    [Feature] 네트워크 상태 검증 기능 추가 (#222)
    * feat: 네트워크 상태 검증 기능 추가
    
    * chore: 코드래빗 리뷰 반영

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -4,3 +4,11 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  if (typeof navigator !== 'undefined' && !navigator.onLine) {
+    return Promise.reject(new axios.CanceledError('오프라인 상태입니다.'));
+  }
+
+  return config;
+});
