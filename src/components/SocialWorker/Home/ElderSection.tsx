77df838f9766732d6e10992ac584e3b1470c4ce8
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-12-06 20:43:05 +0900

    style: 사회복지사 홈화면 어르신 리스트 패딩 스타일 수정

--- src/components/SocialWorker/Home/ElderSection.tsx
+++ src/components/SocialWorker/Home/ElderSection.tsx
@@ -78,8 +78,12 @@ const Elder = styled.div`
     align-items: center;
     justify-content: space-between;
 
-    padding-bottom: 12px;
+    padding: 12px 0px;
     border-bottom: 1px solid ${({ theme }) => theme.colors.gray50};
+
+    &:first-child {
+      padding-top: 0px;
+    }
 
     &:last-child {
       padding-bottom: 0px;
